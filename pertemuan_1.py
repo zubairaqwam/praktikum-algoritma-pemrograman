cf1e13a8046c89f02a30e23c2bc62e95e547f296
Zubair Aqwam <zubairaqwamibadurrohman@smkitihsanulfikri.sch.id>
2026-08-27 18:32:34 +0700

    Upload File Praktikum

---
+++ pertemuan_1.py
@@ -0,0 +1,17 @@
+# Menampilkan header/judul biodata
+print("=================================")
+print("       Biodata Diri")
+print("=================================")
+# Data diri disimpan dalam variabel seuiai dengan tipe data
+nama = "Zubair Aqwam 'Ibadurrohman"
+NIM = 2605060026
+TTL = "Magelang, 16 April 2008"
+gender = "Laki-laki"
+hobi = "Olahraga"
+# Menampilkan data diri menggunakan fungsi print()
+print("Nama\t\t\t:", nama)
+print("NIM\t\t\t\t:", NIM)# /t digunakan untuk membuat jarak/tab
+print("Tempat, \nTanggal Lahir\t:", TTL)
+print("Jenis Kelamin\t:", gender)
+print("Hobi\t\t\t:", hobi)
+print("\n===============================")# /n digunakan untuk membuat baris baru

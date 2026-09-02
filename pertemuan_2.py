90a0866f2c9659f775dd19e71da09985d18fec5d
Zubair Aqwam <zubairaqwamibadurrohman@smkitihsanulfikri.sch.id>
2026-09-02 21:41:13 +0700

    Upload Praktikum 2

---
+++ pertemuan_2.py
@@ -0,0 +1,36 @@
+# SOAL 1
+nama = "Zubair Aqwam 'Ibadurrohman"
+umur = 18
+berat = 50.5
+print("Nama :", nama)
+print("Umur :", umur, "tahun")
+print("Berat Badan :", berat, "kg")
+
+# SOAL 2
+angka_string = "123"
+angka_float = 45.67
+angka_integer = 89
+# 1. Konversi angka_string menjadi integer
+str_to_int = int(angka_string)
+# 2. Konversi angka_float menjadi integer
+float_to_int = int(angka_float)
+# 3. Konversi angka_integer menjadi float
+int_to_float = float(angka_integer)
+# 4. Konversi angka_integer menjadi string
+int_to_str = str(angka_integer)
+
+print("Konversi angka_string menjadi integer:", str_to_int)
+print("Konversi angka_float menjadi integer:", float_to_int)
+print("Konversi angka_integer menjadi float:", int_to_float)
+print("Konversi angka_integer menjadi string:", int_to_str)
+
+# SOAL 3
+usia = int(input("Masukan usia: "))
+tinggi = float(input("Masukan tinggi badan (cm): "))
+nama = input("Masukan nama: ")
+
+print("\n=== Data yang Dimasukkan ===")
+print("Nama          :", nama)
+print("Usia          :", usia, "tahun")
+print("Tinggi Badan  :", tinggi, "cm")
+

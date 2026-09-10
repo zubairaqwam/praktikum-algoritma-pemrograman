63e1df392d118373307ebb86482b417330ee5400
Zubair Aqwam <zubairaqwamibadurrohman@smkitihsanulfikri.sch.id>
2026-09-10 15:34:09 +0700

    Add files via upload

---
+++ 0026_Zubair_Operasi_Aritmatika.py
@@ -0,0 +1,17 @@
+# Kompone yang Diketahui
+panjang = 12
+lebar = 5
+tinggi = 8
+# Soal Menghitung luas, Keliling dan Volume Bangunan (Balok)
+luas = 2 * (panjang *lebar + panjang *tinggi + lebar*tinggi)
+volume = panjang  * lebar * tinggi
+keliling = 4 * (panjang + lebar + tinggi)
+# Menampilkan hasil Operasi
+print("Luas adalah =", luas)
+print("Volume adalah =", volume)
+print("Keliling adalah =", keliling)
+# Operasi Komperasi
+hasil = luas > 50
+print("Apakah luas lebih dari 50? :", hasil)
+hasil1 = volume == 480
+print("Apakah volume  bernilai 480? :", hasil1)
